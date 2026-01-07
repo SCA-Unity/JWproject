3926655b74aef43f0926f650ea3a21b2606658f2
today747 <junusong8@gmail.com>
2026-01-07 20:50:32 +0900

    맵
    1234

---
+++ Fallen-Abyss/Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [Header("이동할 씬 이름")]
+    [SerializeField] private string nextSceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
