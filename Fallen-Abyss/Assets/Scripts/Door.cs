8d3fc91729b3b6c5242730c54b43b9deeea6cb24
today747 <junusong8@gmail.com>
2026-01-16 20:49:25 +0900

    맵
    223123

--- Fallen-Abyss/Assets/Scripts/Door.cs
+++ Fallen-Abyss/Assets/Scripts/Door.cs
@@ -1,37 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Elevator : MonoBehaviour
+public class Door : MonoBehaviour
 {
-    [Header("엘리베이터 설정")]
-    public float moveY = 5f;   // 인스펙터에서 입력
-    public float speed = 2f;   // 인스펙터에서 입력
+    [Header("이동할씬 이름")]
+    [SerializeField] private string nextSceneName;
 
-    private Vector3 startPos;
-    private Vector3 targetPos;
-    private bool isMoving = false;
-
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        startPos = transform.position;
-        targetPos = startPos + new Vector3(0f, moveY, 0f);
-    }
-
-    void Update()
-    {
-        if (!isMoving) return;
-
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            targetPos,
-            speed * Time.deltaTime
-        );
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            isMoving = true;
+            SceneManager.LoadScene(nextSceneName);
         }
     }
 }
