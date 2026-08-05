0dc33d9e312d4019fdf210c704160c10bbaf1fc9
songjw08 <junusong8@gmail.com>
2026-08-05 20:48:43 +0900

    다이렉션 설정
    123123

--- Fallen-Abyss/Assets/Scripts/Boss_AttackEventsReceiver.cs
+++ Fallen-Abyss/Assets/Scripts/Boss_AttackEventsReceiver.cs
@@ -3,36 +3,47 @@ using TwoBitMachines.FlareEngine;
 
 public class Boss_AttackEventsReceiver : MonoBehaviour
 {
-    [Header("Boss Melee")]
-    public Melee melee;
+    [SerializeField] private Melee melee;
+    [SerializeField] private Collider2D hitbox;
+
+    private void Awake()
+    {
+        if (melee == null)
+            melee = GetComponent<Melee>();
+
+        if (melee == null)
+            melee = GetComponentInParent<Melee>();
+
+        if (hitbox != null)
+            hitbox.enabled = false;
+    }
 
     // 공격 시작
     public void StartAttack()
     {
-        Debug.Log("보스 공격 시작");
+        Debug.Log("Boss Attack Start");
     }
 
-    // 무기 판정 ON
+    // 히트박스 ON
     public void EnableHitbox()
     {
-        Debug.Log("Hitbox ON");
-        // swordCollider.enabled = true;
+        if (hitbox != null)
+            hitbox.enabled = true;
     }
 
-    // 무기 판정 OFF
+    // 히트박스 OFF
     public void DisableHitbox()
     {
-        Debug.Log("Hitbox OFF");
-        // swordCollider.enabled = false;
+        if (hitbox != null)
+            hitbox.enabled = false;
     }
 
-    // 공격 종료
+    // 공격 종료 (Flare Engine에 알림)
     public void CompleteAttack()
     {
-        if (melee == null)
-            melee = GetComponentInParent<Melee>();
-
         if (melee != null)
             melee.CompleteAttack();
+
+        Debug.Log("Boss Attack Complete");
     }
 }
